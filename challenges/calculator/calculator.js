ef577d51b98128385c1b51ea5ab1987a5fd251e5
arslonbekXX <code.2307sabia@gmail.com>
2024-11-25 15:08:17 +0500

    lesson-17 start-codes

--- challenges/calculator/calculator.js
+++ challenges/calculator/calculator.js
@@ -1,7 +1,9 @@
 function calculator(num1, num2, operation) {
- // Kodni shu yerda yozing
+	return num1 + num2;
 }
 
 // Masalan:
-// console.log(calculator(10, 5, '+')); // 15
-// console.log(calculator(10, 5, '*')); // 50
+console.log(calculator(10, 5, '+')); // 15
+console.log(calculator(10, 5, '*')); // 50
+console.log(calculator(10, 5, '-')); // 5
+console.log(calculator(12, 5, '/')); // 2.4
