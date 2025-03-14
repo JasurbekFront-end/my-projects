1f7caa36f39b1e54fb367ac230fcf4c7982ad446
arslonbekXX <code.2307sabia@gmail.com>
2025-03-14 15:28:58 +0500

    lesson-62 start-codes

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-export {}
+import type { Question } from './types';
+import { renderQuiz } from './ui';
+import { generateQuestion } from './utils';
+const questions: Question[] = [];
+
+export function init() {
+  const question = generateQuestion();
+  questions.push(question);
+  renderQuiz(question);
+}
+
+init();
